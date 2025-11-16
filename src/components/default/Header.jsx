bbae5bbf2519c7c9295c5cc6bfc0bf63a4afbfec
lucas-faleiro <lucas.faleirosilva@gmail.com>
2025-11-16 15:30:35 -0300

    Add: routes to / and login

--- src/components/default/Header.jsx
+++ src/components/default/Header.jsx
@@ -1,11 +1,16 @@
+import { Link } from "react-router";
 import Icon from "../custom/Icon";
 
 export default function Header() {
   return (
     <div className="flex justify-between items-center py-4 px-14 border-b-2 border-pink-200 bg-bg-header text-pink-200">
-      <h1 className="font-[Emilys_Candy] text-4xl italic">Chocolaterie</h1>
+      <Link to="/">
+        <h1 className="font-[Emilys_Candy] text-4xl italic">Chocolaterie</h1>
+      </Link>
       <div className="flex items-center gap-4">
-        <Icon className="fa-regular fa-user text-2xl mr-4 cursor-pointer" />
+        <Link to="/login" className="hover:text-pink-400">
+          <Icon className="fa-regular fa-user text-2xl mr-4 cursor-pointer" />
+        </Link>
         <Icon className="fa-solid fa-cart-shopping text-2xl cursor-pointer" />
       </div>
     </div>

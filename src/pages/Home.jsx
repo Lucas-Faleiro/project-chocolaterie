9e9481bde696d46bcf7662d6eb78ca37baf09bec
Lucas Faleiro <lucas.faleirosilva@gmail.com>
2025-11-17 16:05:01 -0300

    Add: mock products data to fetch and loading state

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,22 @@
 import { ProductCard, Header, SearchBar } from "../components";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const chocolateListTemplate = [
-    {
-      id: 1,
-      name: "Chocolate ao Leite com toque de amêndoas",
-      price: "10,00",
-      img: "chocolate-ao-leite.jpg",
-    },
-    {
-      id: 2,
-      name: "Chocolate 50% com café",
-      price: "12,00",
-      img: "chocolate-cafe.jpg",
-    },
-    {
-      id: 3,
-      name: "Chocolate 70% com castanhas do pará",
-      price: "15,00",
-      img: "chocolate-castanhas.jpg",
-    },
-  ];
+  const [chocolateList, setChocolateList] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    try {
+      setLoading(true);
+    fetch("/data/products.json")
+      .then((response) => response.json())
+      .then((data) => setChocolateList(data));
+    } catch (error) {
+      console.error("Erro ao buscar produtos:", error);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
 
   return (
     <div>
@@ -33,7 +29,7 @@ export default function Home() {
         inputclass="border-2 border-pink-200 rounded-full px-4 py-2 w-1/2 focus:outline-none focus:border-pink-400 shadow-md "
       />
       <div className="md:grid-cols-2 md:gap-8 lg:grid-cols-3 grid-cols-1 mt-10 grid justify-items-center container mx-auto">
-        {chocolateListTemplate.map((chocolate) => {
+        {loading ? <p>Carregando Produtos...</p> : chocolateList.map((chocolate) => {
           return (
             <ProductCard
               key={chocolate.id}

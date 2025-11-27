6912fc0b36ae758ff09b6966b7d0f965133256eb
Lucas Faleiro <lucas.faleirosilva@gmail.com>
2025-11-27 17:54:49 -0300

    Refactor: improve ShopCart layout

--- src/pages/ShopCart.jsx
+++ src/pages/ShopCart.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { Header } from "../components";
+import { Header, Icon } from "../components";
 import ShopCartContext from "../context/ShopCartContext";
 
 function ShopCart() {
@@ -16,17 +16,25 @@ function ShopCart() {
           <p className="text-center mt-5 text-[1.4rem]">Seu carrinho está vazio.</p>
         </div>
       ) : (
-      <div className="grid grid-cols-2  gap-8 container mx-auto"> 
+      <div className="grid grid-cols-2 gap-4 mt-10 container mx-auto "> 
         {/* <p>Meu Carrinho</p> */}
         {cartItems.map((item) => (
-          <div key={item.id} className="col-start-1">
+          <div key={item.id} className="col-start-1 flex gap-2 relative border-2 border-pink-200 rounded-lg px-4 py-4 shadow-md">
             <img src={`/images/${item.img}`} alt={item.name} className="w-32 h-32 object-cover rounded-lg" />
-            <h2 className="text-xl font-bold mt-4">{item.name}</h2>
-            <p className="text-pink-400">R${item.price}</p>
+            <div>
+              <h4 className="font-bold">{item.name}</h4>
+              <span className="text-pink-400">R${item.price}</span>
+              <div className="flex items-center gap-2"> 
+                <Icon className="fa-solid fa-circle-minus text-lg cursor-pointer hover:text-gray-400" />
+                <input type="number" name="qty" id="qty" value={1} className="w-[1.4rem] h-[1.4rem] border-2 border-gray-200" />
+                <Icon className="fa-solid fa-circle-plus text-lg cursor-pointer hover:text-gray-400" />
+              </div>
+            </div>
+            <Icon className="fa-solid fa-trash text-xl mt-4 cursor-pointer hover:text-gray-400 absolute right-4 top-2 text-gray-300" />
         </div>
       ))}
-        <div className="border-2 border-pink-200 rounded-lg p-4 h-fit mt-10 col-start-2 row-start-1">
-          <p>Resumo das Compras</p>
+        <div className="border-2 border-pink-200 rounded-lg p-4 h-fit col-start-2 row-start-1">
+          <span>Resumo das Compras</span>
         </div>
       </div> 
     )}

64c98c85a11eb46dd7c77f0d813ec81a793d231b
Lucas Faleiro <lucas.faleirosilva@gmail.com>
2025-12-03 16:41:03 -0300

    Refactor: restructure Database service and add findById

--- src/services/Database.js
+++ src/services/Database.js
@@ -1,8 +1,14 @@
 import supabase from "./SupabaseClient";
 
-const Database = async (table, fields) => {
-  let response = await supabase.from(table).select(fields);
-  return response;
+const Database = {
+  fetch: async (table, fields) => {
+    let response = await supabase.from(table).select(fields);
+    return response;
+  },
+  findById: async (table, fields, id) => {
+    let response = await supabase.from(table).select(fields).eq("id", id);
+    return response;
+  },
 };
 
 export default Database;

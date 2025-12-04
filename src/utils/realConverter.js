73fbb38f73dcecce1382b05d9883a5dd22ec99bb
Lucas Faleiro <lucas.faleirosilva@gmail.com>
2025-12-04 15:38:47 -0300

    refactor: takes off middle space cause i think its better looking

--- src/utils/realConverter.js
+++ src/utils/realConverter.js
@@ -1,12 +1,12 @@
 const realConverter = (value) => {
-    const number = Number(value) || 0;
-    return new Intl.NumberFormat('pt-br', {
-        style: 'currency',
-        currency: 'BRL',
-        minimumFractionDigits: 2,
-        maximumFractionDigits: 2,
-    }).format(number)
-}
+  const number = Number(value) || 0;
+  const formatted = new Intl.NumberFormat("pt-br", {
+    style: "currency",
+    currency: "BRL",
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(number);
+  return formatted.replace(/\s/g, "");
+};
 
 export default realConverter;
-
